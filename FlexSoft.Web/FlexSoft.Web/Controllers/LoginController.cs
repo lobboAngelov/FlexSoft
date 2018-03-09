3f8b1f8a25836f3d4645e62908df121c910af0f9
Lyuboslav Angelov <l.angelov96@gmail.com>
2018-03-09 17:07:21 +0200

    kur

---
+++ FlexSoft.Web/FlexSoft.Web/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FlexSoft.Web.Models;
+
+namespace FlexSoft.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View(new LoginInfo());
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginInfo loginInfo)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ FlexSoft.Web/FlexSoft.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FlexSoft.Web.MvcApplication" Language="C#" %>
