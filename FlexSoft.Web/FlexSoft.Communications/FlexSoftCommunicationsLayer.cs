b8698b4299ef7ad4feee243afb843c05d34b56b9
Lyuboslav Angelov <langelov@melontech.com>
2018-03-26 18:17:28 +0300

    Client opened

--- FlexSoft.Web/FlexSoft.Communications/FlexSoftCommunicationsLayer.cs
+++ FlexSoft.Web/FlexSoft.Communications/FlexSoftCommunicationsLayer.cs
@@ -9,7 +9,8 @@ namespace FlexSoft.Communications
         private readonly IWebSocketServer _webSocketServer;
         private readonly ConcurrentDictionary<IWebSocketConnection, ISocketClient> _joinedClients;
 
-        public FlexSoftCommunicationsLayer(IWebSocketServer webSocketServer, ConcurrentDictionary<IWebSocketConnection, ISocketClient> joinedClients)
+        public FlexSoftCommunicationsLayer(IWebSocketServer webSocketServer,
+            ConcurrentDictionary<IWebSocketConnection, ISocketClient> joinedClients)
         {
             _webSocketServer = webSocketServer;
             _joinedClients = joinedClients;
@@ -30,12 +31,20 @@ namespace FlexSoft.Communications
 
         public void SocketClientOpened(IWebSocketConnection connection)
         {
-            throw new NotImplementedException();
+            var clientInfo = _joinedClients[connection].ToString();
+            Console.WriteLine($"Client connected: {clientInfo}");
         }
 
         public void SocketMessage(IWebSocketConnection connection, byte[] data)
         {
-            throw new NotImplementedException();
+            if (_joinedClients.ContainsKey(connection))
+            {
+
+            }
+            else
+            {
+                
+            }
         }
 
         public void StartServerClientSocket()
